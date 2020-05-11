2da28985e411edc3e501acc33ca0222b38931c3e
Andre Lucas Silva Freires <andre_ccopias@hotmail.com>
2020-05-11 14:36:08 -0400

    Criacao das entidades.

---
+++ QuickBuy.Dominio/Entidades/ObjetoDeValor/FormaPagamento.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Entidades.ObjetoDeValor
+{
+    public class FormaPagamento
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+
+        public bool EhBoleto {
+            get
+            {
+                return Id == (int) Enumerados.TipoFormaPagamentoEnum.Boleto; 
+            }
+        }
+        public bool EhCartaoCredito
+        {
+            get
+            {
+                return Id == (int)Enumerados.TipoFormaPagamentoEnum.CartaoCredito;
+            }
+        }
+        public bool EhDeposito
+        {
+            get
+            {
+                return Id == (int)Enumerados.TipoFormaPagamentoEnum.Deposito;
+            }
+        }
+        public bool NaoFoiDefinido
+        {
+            get
+            {
+                return Id == (int)Enumerados.TipoFormaPagamentoEnum.NaoDefinido;
+            }
+        }
+    }
+}
